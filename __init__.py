6c299107b1d1805cb858787d780931f69fdf0f39
Alexis Rolland <alexis.rolland@ubisoft.com>
2024-07-03 00:17:36 +0800

    Add support for batch of images

--- __init__.py
+++ __init__.py
@@ -52,26 +52,43 @@ class RunAuraSR:
             }
         }
 
+    def tensor2pil(self, image):
+        batch_count = image.size(0) if len(image.shape) > 3 else 1
+        if batch_count > 1:
+            out = []
+            for i in range(batch_count):
+                out.extend(self.tensor2pil(image[i]))
+            return out
+        return [Image.fromarray(np.clip(255.0 * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))]
+
+    def pil2tensor(self, image):
+        if isinstance(image, list):
+            return torch.cat([self.pil2tensor(img) for img in image], dim=0)
+        return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+
     def execute(self, AURASR_MODEL, IMAGE,):
         # Convert tensor to PIL image
-        image = Image.fromarray(np.clip(255. * IMAGE.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+        images = self.tensor2pil(IMAGE)
 
-        # Upscale
-        upscaled_image = AURASR_MODEL.upscale_4x(image)
+        # Upscale images
+        upscaled_images = list()
+        for image in images:
+            upscaled_images.append(AURASR_MODEL.upscale_4x(image))
 
-        # Convert PIL image to tensor
-        tensor = torch.from_numpy(np.array(upscaled_image ).astype(np.float32) / 255.0).unsqueeze(0)
-        return (tensor,)
+        # Convert PIL images to tensor
+        upscaled_images = self.pil2tensor(upscaled_images)
+
+        return (upscaled_images,)
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "downloadAuraSR": downloadAuraSR,
-    "RunAuraSR": RunAuraSR,
+    "RunAuraSR": RunAuraSR
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "downloadAuraSR": "Download AuraSR",
-    "RunAuraSR": "Run AuraSR",
+    "RunAuraSR": "Run AuraSR"
 }
